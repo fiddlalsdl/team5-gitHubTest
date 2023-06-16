32a23baca8a0f3f1fd7afdc33c9476375da11aa8
jaehyun0306 <fexis1594@gmail.com>
2023-06-16 15:17:27 +0900

    Docs User

--- demo/src/main/java/com/example/demo/dto/User.java
+++ demo/src/main/java/com/example/demo/dto/User.java
@@ -1,7 +1,13 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
 	private int Age;
 	private String name;
+	private int height;
 }
